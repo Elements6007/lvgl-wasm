0826324bb905ed3897a3124aeff6e9c4c2c55a9a
lupyuen <luppy@appkaki.com>
2020-08-13 17:50:53 +0800

    Fix package

---
+++ docs/rust/pkg/README.md
@@ -0,0 +1,7 @@
+# Rust WebAssembly Modules for LVGL
+
+Based on...
+
+https://rustwasm.github.io/docs/wasm-bindgen/examples/julia.html
+
+https://rustwasm.github.io/docs/wasm-bindgen/examples/paint.html
---
+++ docs/rust/pkg/lvgl_wasm_rust_bg.d.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function draw(a: number, b: number, c: number, d: number, e: number): void;
+export function __wbindgen_exn_store(a: number): void;

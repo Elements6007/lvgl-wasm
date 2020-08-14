4d73e892625f6e5905bb2e2b17f6dee49659d967
lupyuen <luppy@appkaki.com>
2020-08-14 14:16:17 +0800

    Build

--- wasm/lv_port_disp.c
+++ wasm/lv_port_disp.c
@@ -137,7 +137,7 @@ static void disp_init(void)
 }
 
 //  Defined in lvgl.c
-void put_display_px(uint16_t x, uint16_t y, uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+void put_display_px(uint16_t x, uint16_t y, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 
 /* Flush the content of the internal buffer the specific area on the display
  * You can use DMA or any hardware acceleration to do this operation in the background but

45ed4860938d9caf890adb462b950a10aa3bebc8
lupyuen <luppy@appkaki.com>
2020-08-14 11:08:20 +0800

    Added display functions

--- wasm/test_rust.c
+++ wasm/test_rust.c
@@ -1,11 +1,85 @@
-/* Sample App:
-emcc wasm/test_rust.c -s WASM=1 -o wasm/test_rust.html rust/pkg/lvgl_wasm_rust_bg.wasm
+/* Sample app to test C and Rust WebAssembly Interoperability
+Build sample Rust app: wasm/test_rust.html, test_rust.js, test_rust.wasm
+emcc \
+    -g \
+    wasm/test_rust.c \
+    -s WASM=1 \
+    -s "EXPORTED_FUNCTIONS=[ '_main', '_test_c', '_test_c_set_buffer', '_test_c_get_buffer', '_test_c_buffer_address', '_test_rust', '_test_rust2', '_test_rust3', '_test_rust_set_buffer', '_test_rust_get_buffer' ]" \
+    -o wasm/test_rust.html \
+    target/wasm32-unknown-emscripten/debug/liblvgl_wasm_rust.a
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <assert.h>
+#include "../lvgl.h"
+
+////////////////////////////////////////////////////////////////////
+//  Device and Display Buffers
+
+///  RGBA Display Buffer for HTML Canvas
+#define DISPLAY_BYTES_PER_PIXEL 4
+uint8_t display_buffer[LV_HOR_RES_MAX * LV_VER_RES_MAX * DISPLAY_BYTES_PER_PIXEL];
+
+///  Plot a pixel on the PineTime Device Buffer
+void put_device_px(uint16_t x, uint16_t y, uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+    assert(x >= 0); assert(x < LV_HOR_RES_MAX);
+    assert(y >= 0); assert(y < LV_VER_RES_MAX);
+    //  TODO: Map RGB565 to RGBA
+    int i = (y * LV_HOR_RES_MAX * DISPLAY_BYTES_PER_PIXEL) + (x * DISPLAY_BYTES_PER_PIXEL);
+    display_buffer[i++] = r;  //  Red
+    display_buffer[i++] = g;  //  Green
+    display_buffer[i++] = b;  //  Blue
+    display_buffer[i++] = a;  //  Alpha
+}
+
+///  Return the WebAssembly Address of the Display Buffer
+unsigned get_display_buffer(void) {
+    uint8_t *p = &test_rust_buffer[0];
+    return (unsigned) p;
+}
+
+///  Return the width of the Display Buffer
+unsigned get_display_width(void) { return LV_HOR_RES_MAX; }
+
+///  Return the height of the Display Buffer
+unsigned get_display_height(void) { return LV_VER_RES_MAX; }
+
+///  Render a box to the Display Buffer
+int test_display(void) {
+    puts("In C: test_display()");
+    for (uint16_t x = 0; x < 20; x++) {
+        for (uint16_t y = 0; y < 20; y++) {     
+            uint8_t r = x % 0xff;
+            uint8_t g = y % 0xff;
+            uint8_t b = ((x + y) / 2) % 0xff;
+            uint8_t a = 0xff;
+            put_device_px(x, y, r, g, b, a);
+        }
+    }
+    return 0;
+}
+
+/*
+///  RGB565 Device Buffer for PineTime
+#define DEVICE_BYTES_PER_PIXEL 3  //  TODO: Switch to RGB565
+uint8_t device_buffer[LV_HOR_RES_MAX * LV_VER_RES_MAX * DEVICE_BYTES_PER_PIXEL];
+
+///  Plot a pixel on the PineTime Device Buffer
+void put_device_px(uint16_t x, uint16_t y, uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
+    assert(x >= 0); assert(x < LV_HOR_RES_MAX);
+    assert(y >= 0); assert(y < LV_VER_RES_MAX);
+    int i = (y * LV_HOR_RES_MAX * DEVICE_BYTES_PER_PIXEL) + (x * DEVICE_BYTES_PER_PIXEL);
+    //  TODO: Switch to RGB565
+    device_buffer[i++] = r;  //  Red
+    device_buffer[i++] = g;  //  Green
+    device_buffer[i++] = b;  //  Blue
+}
+*/
+
+////////////////////////////////////////////////////////////////////
+//  Test
 
 //  Defined in rust/src/lib.rs
 int test_rust(void);

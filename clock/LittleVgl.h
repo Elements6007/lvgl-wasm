db2dab9e55dcfacb9895d6cb7f362a3ffe08807f
Lee Lup Yuen <luppy@appkaki.com>
2020-08-15 13:53:31 +0800

    Fix

--- clock/LittleVgl.h
+++ clock/LittleVgl.h
@@ -6,6 +6,14 @@
 ////#include <drivers/St7789.h>
 ////#include <drivers/Cst816s.h>
 
+////Begin
+namespace Pinetime { 
+  namespace Drivers {
+    class St7789 {};
+    class Cst816S {};
+  } 
+}
+////End
 
 static void disp_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p);
 static bool touchpad_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data);

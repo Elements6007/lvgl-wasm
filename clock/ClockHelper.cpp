5d62fea700402771e8b0c5de410096954e8464f2
Lee Lup Yuen <luppy@appkaki.com>
2020-08-15 13:47:51 +0800

    Copied LittleVgl.cpp

--- clock/ClockHelper.cpp
+++ clock/ClockHelper.cpp
@@ -1,4 +1,5 @@
 ////  WebAssembly Helper
+#include <stdio.h>
 #include <assert.h>
 #include "Clock.h"
 #include "ClockHelper.h"
@@ -11,6 +12,7 @@ static Pinetime::Controllers::Ble *bleController0 = 0;
 
 /// Create an instance of the clock
 int create_clock(void) {
+    puts("In C++: Creating clock...");
     app0 = new DisplayApp();
     dateTimeController0 = new Pinetime::Controllers::DateTime();
     batteryController0 = new Pinetime::Controllers::Battery();
@@ -26,6 +28,7 @@ int create_clock(void) {
 
 /// Redraw the clock
 int refresh_clock(void) {
+    puts("In C++: Refreshing clock...");
     assert(clock0 != 0);
     clock0->Refresh();
     return 0;
@@ -33,6 +36,7 @@ int refresh_clock(void) {
 
 /// Update the clock time
 int update_clock(void) {
+    puts("In C++: Updating clock...");
     assert(clock0 != 0);
     //  TODO
     return 0;

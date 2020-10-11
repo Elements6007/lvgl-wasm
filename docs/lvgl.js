b4f43f1090d3d881993b7fbb486defcfa4617797
lupyuen <luppy@appkaki.com>
2020-10-11 02:46:27 +0000

    Commit from GitHub Actions

--- docs/lvgl.js
+++ docs/lvgl.js
@@ -1152,9 +1152,9 @@ function updateGlobalBufferAndViews(buf) {
   Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
 }
 
-var STACK_BASE = 5762720,
+var STACK_BASE = 5756000,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 519840;
+    STACK_MAX = 513120;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 

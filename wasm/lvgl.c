321dee48199b6bae51256b1e2077a88208a43284
Lee Lup Yuen <luppy@appkaki.com>
2020-10-23 07:26:40 +0800

    Clean up

--- wasm/lvgl.c
+++ wasm/lvgl.c
@@ -1,6 +1,5 @@
 //  Simple port of LVGL to WebAssembly.
 //  Renders UI controls to HTML Canvas but touch input not handled yet.
-//  To build see lvgl.sh
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,7 +19,6 @@ uint8_t display_buffer[LV_HOR_RES_MAX * LV_VER_RES_MAX * DISPLAY_BYTES_PER_PIXEL
 void put_display_px(uint16_t x, uint16_t y, uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
     assert(x >= 0); assert(x < LV_HOR_RES_MAX);
     assert(y >= 0); assert(y < LV_VER_RES_MAX);
-    //  TODO: Map RGB565 to RGBA
     int i = (y * LV_HOR_RES_MAX * DISPLAY_BYTES_PER_PIXEL) + (x * DISPLAY_BYTES_PER_PIXEL);
     display_buffer[i++] = r;  //  Red
     display_buffer[i++] = g;  //  Green
@@ -88,69 +86,19 @@ void render_display(void) {
 }
 
 ////////////////////////////////////////////////////////////////////
-//  Rust LVGL Interface
-
-/// Create LVGL Screen. Called by rust/wasm/src/lib.rs
-lv_obj_t *get_screen(void) {
-    puts("In C: Getting screen...");
-    lv_obj_t *screen = lv_scr_act();
-    return screen;
-}
-
-////////////////////////////////////////////////////////////////////
-//  RIOT Interface
-
-/// Month names. From https://github.com/lupyuen/pinetime-rust-riot/blob/master/modules/controller/controller_time.c
-#define MONSPERYEAR     12
-static const char *mon_short_names[MONSPERYEAR + 1] = {
-    [0] = "Inv",
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sept",
-    "Oct",
-    "Nov",
-    "Dec",
-};
-
-/// Date time format. From https://github.com/lupyuen/pinetime-rust-riot/blob/master/modules/controller/include/controller/time.h
-typedef struct {
-    uint16_t year;
-    uint8_t month;
-    uint8_t dayofmonth;
-    uint8_t hour;
-    uint8_t minute;
-    uint8_t second;
-    uint8_t fracs;
-} controller_time_spec_t;
-
-/// Get month name. From https://github.com/lupyuen/pinetime-rust-riot/blob/master/modules/controller/controller_time.c
-const char *controller_time_month_get_short_name(controller_time_spec_t *time) {
-    assert(time->month < MONSPERYEAR);
-    return mon_short_names[time->month];
-}
-
-////////////////////////////////////////////////////////////////////
-//  LVGL Compatibility
-
-/// Change LVGL v6 lv_obj_set_style() to LVGL v7 lv_obj_reset_style_list() and lv_obj_add_style()
-/// Called by rust/app/src/watch_face.rs
-#undef lv_obj_set_style
-void lv_obj_set_style(lv_obj_t *obj, const lv_style_t *style) {
-    lv_obj_reset_style_list(obj, LV_LABEL_PART_MAIN);  //  TODO: Handle non-label styles
-    lv_obj_add_style(obj, LV_LABEL_PART_MAIN, style);  //  TODO: Handle non-label styles
-}
-
-////////////////////////////////////////////////////////////////////
 //  Main
 
 /// Do nothing
 int main(int argc, char **argv) {
     puts("In C: main()");
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////
+//  Debugging
+
+/// Print pointer
+int print_pointer(const char *msg, const void *ptr) {
+    printf("%s: %p\n", msg, ptr);
     return 0;
 }

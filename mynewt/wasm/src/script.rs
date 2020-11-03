90bd02c33c47b723e0d30a9f27c04fc44173a236
lupyuen <luppy@appkaki.com>
2020-11-03 13:07:12 +0800

    Add string package

--- mynewt/wasm/src/script.rs
+++ mynewt/wasm/src/script.rs
@@ -3,15 +3,21 @@ use rhai::{
     EvalAltResult,
     RegisterFn,
     //RegisterResultFn,
+    packages::{
+        BasicStringPackage,
+        Package,
+    },
 };
 use barebones_watchface::watchface::{self};   //  Needed for calling WatchFace traits
 
 pub fn run_script() -> Result<(), Box<EvalAltResult>> {
     let mut engine = Engine::new();
+    let package = BasicStringPackage::new();
+    engine.load_package(package.get());
     engine.register_fn("watchface_get_active_screen", watchface::get_active_screen);
     //let result = engine.eval::<i64>("watchface_get_active_screen()")?;
 
-    let result = engine.eval::<i64>("40 + 2")?;
+    let result = engine.eval::<i64>("let a = watchface_get_active_screen(); print(a); 40 + 2")?;
     println!("Answer: {}", result);             // prints 42
     Ok(())
 }

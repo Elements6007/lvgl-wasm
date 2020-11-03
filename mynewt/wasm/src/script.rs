fba88fc610fa59cfc8988f3836a7627ee7f26d54
lupyuen <luppy@appkaki.com>
2020-11-03 12:24:20 +0800

    Register function

--- mynewt/wasm/src/script.rs
+++ mynewt/wasm/src/script.rs
@@ -1,11 +1,17 @@
-use rhai::{Engine, EvalAltResult};
+use rhai::{
+    Engine,
+    EvalAltResult,
+    RegisterFn,
+    //RegisterResultFn,
+};
+use barebones_watchface::watchface::{self};   //  Needed for calling WatchFace traits
 
 pub fn run_script() -> Result<(), Box<EvalAltResult>> {
-    let engine = Engine::new();
+    let mut engine = Engine::new();
+    engine.register_fn("watchface_get_active_screen", watchface::get_active_screen);
+    //let result = engine.eval::<i64>("watchface_get_active_screen()")?;
 
     let result = engine.eval::<i64>("40 + 2")?;
-
     println!("Answer: {}", result);             // prints 42
-
     Ok(())
 }

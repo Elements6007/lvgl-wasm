aa127958b90f9320a6c52dd20345b04e9ee92843
lupyuen <luppy@appkaki.com>
2020-11-04 16:34:06 +0800

    Fixing label

--- mynewt/wasm/src/script.rs
+++ mynewt/wasm/src/script.rs
@@ -50,14 +50,27 @@ pub fn run_script() -> Result<(), Box<EvalAltResult>> {
     engine.register_fn("ptr_null", ptr_null);  //  TODO: Rewrite as ptr::null
     engine.register_result_fn("label_create", label_create);  //  TODO: Rewrite as label::create
     engine.register_result_fn("label_set_text", label_set_text);  //  TODO: Rewrite as label::set_text
-    //  engine.register_result_fn("obj_set_width", obj::set_width);
-    //  engine.register_result_fn("obj_set_height", obj::set_height);
+    engine.register_result_fn("obj_set_width", obj_set_width);  //  TODO: Rewrite as obj_set_width
+    engine.register_result_fn("obj_set_height", obj_set_height);  //  TODO: Rewrite obj_set_height
 
     //  Execute the Rhai script
     let result = engine.eval::<i64>(r#"
         //  Here is the Rhai script (looks like Rust), which can be modified at runtime...
-        //  Call an LVGL function
+
+        //  Call an LVGL function to get the LVGL active screen
         let screen = watchface_get_active_screen();
+
+        //  Create an LVGL label
+        //  let lbl = label_create(screen, ptr_null()) ? ;
+
+        //  Set the text of the LVGL label
+        //  label_set_text(lbl, "TODO") ? ;  //  TODO: Rewrite as strn!(...)
+
+        //  Set the label width
+        //  obj_set_width(lbl, 240) ? ;
+
+        //  Set the label height
+        //  obj_set_height(lbl, 200) ? ;
 
         //  Print a message
         print("Hello from Rhai script in WebAssembly!");
@@ -76,23 +89,39 @@ fn ptr_null() -> *const obj::lv_obj_t {
 //  LVGL Functions mapped to Rhai calling convention
 //  TODO: Generate automatically with the `safe_wrap` proc macro
 
+/// Create a label
 fn label_create(
     par: *mut obj::lv_obj_t, 
     copy: *const obj::lv_obj_t
 ) -> Result<Dynamic, Box<EvalAltResult>> {
     let result = label::create(par, copy)
         .expect("label_create fail");
-    //  TODO: Ok(result.into())
-    Ok(().into())
+    Ok(Dynamic::from(result))
 }
 
+/// Set label text
 fn label_set_text(lbl: lvgl::Ptr, _s: &str) -> Result<Dynamic, Box<EvalAltResult>> {    
     let result = label::set_text(lbl, macros::strn!("TODO"))
         .expect("label_set_text fail");
     Ok(result.into())
 }
 
+/// Set widget width
+pub fn obj_set_width(obj: *mut obj::lv_obj_t, w: obj::lv_coord_t) -> Result<Dynamic, Box<EvalAltResult>> {
+    let result = obj::set_width(obj, w)
+        .expect("obj_set_width fail");
+    Ok(result.into())
+}
+
+/// Set widget height
+pub fn obj_set_height(obj: *mut obj::lv_obj_t, h: obj::lv_coord_t) -> Result<Dynamic, Box<EvalAltResult>> {
+    let result = obj::set_height(obj, h)
+        .expect("obj_set_height fail");
+    Ok(result.into())
+}
+
 /*
+let screen = watchface::get_active_screen();
 let lbl = label::create(screen, ptr::null()) ? ;  //  `?` will terminate the function in case of error
 label::set_text(     lbl, strn!("00:00")) ? ;     //  strn creates a null-terminated string
 obj::set_width(      lbl, 240) ? ;

65b558ba43c992cbdd4511b01e5320e528acad22
lupyuen <luppy@appkaki.com>
2020-11-08 20:59:20 +0800

    Add script target

--- wasm/copy-wasm.cmd
+++ wasm/copy-wasm.cmd
@@ -1,4 +1,17 @@
-:: Copy outputs to docs/lvgl.js, lvgl.wasm
+:: Copy outputs to docs/lvgl.js, lvgl.wasm, script.js, script.wasm
 
-copy wasm\lvgl.wasm docs\lvgl.wasm
-copy wasm\lvgl.js   docs\lvgl.js
+if exist wasm\lvgl.wasm (
+    copy wasm\lvgl.wasm docs\lvgl.wasm
+)
+
+if exist wasm\lvgl.js (
+    copy wasm\lvgl.js   docs\lvgl.js
+)
+
+if exist wasm\script.wasm (
+    copy wasm\script.wasm docs\script.wasm
+)
+
+if exist wasm\script.js (
+    copy wasm\script.js   docs\script.js
+)
--- wasm/copy-wasm.sh
+++ wasm/copy-wasm.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
-# Copy outputs to docs/lvgl.js, lvgl.wasm
+# Copy outputs to docs/lvgl.js, lvgl.wasm, script.js, script.wasm
 
 # Stop the script on error, echo all commands
 set -e -x
 
-cp wasm/lvgl.wasm docs/lvgl.wasm
-cp wasm/lvgl.js docs/lvgl.js
+if [ -e wasm/lvgl.wasm ]; then
+    cp wasm/lvgl.wasm docs/lvgl.wasm
+fi
+
+if [ -e wasm/lvgl.js ]; then
+    cp wasm/lvgl.js docs/lvgl.js
+fi
+
+if [ -e wasm/script.wasm ]; then
+    cp wasm/script.wasm docs/script.wasm
+fi
+
+if [ -e wasm/script.js ]; then
+    cp wasm/script.js docs/script.js
+fi

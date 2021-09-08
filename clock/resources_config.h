3df97df141e655b35a0743655555d131d9e97818
Elements6007 <88860704+Elements6007v2@users.noreply.github.com>
2021-09-08 14:19:54 -0400

    Create resources_config.h

---
+++ clock/resources_config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Resource version
+#define RESOURCE_VERION 1.0.2
+
+#define WF_BACKGROUND_ANALOG01    "F:/bg_clock_01.bin"
+#define WF_BACKGROUND_ANALOG02    "F:/bg_clock_01_1.bin"
+
+#define WF_BACKGROUND_C1        "F:/bg_clock_02.bin"
+#define WF_BACKGROUND_C2        "F:/bg_clock_03.bin"
+#define WF_BACKGROUND_C3        "F:/bg_clock_04.bin"
